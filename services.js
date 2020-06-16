cb85924420f0a07ddd7a1f9365e99bc482fc99b7
wbr <wbr@wbr.local>
2020-06-16 13:06:16 +0800

    UPDATE

--- services.js
+++ services.js
@@ -2,7 +2,7 @@ module.exports =
 
 [{
 	name: "web",
-	repo: "https://github.com/sharelatex/web-sharelatex.git",
+	repo: "https://github.com/WangBRen/web.git",
 	version: "master"
 }, {
 	name: "real-time",
